2d98d6008f64d6810674de5be465b495b10b0602
dcyr <cyr.dominic@gmail.com>
2018-04-26 12:07:57 -0400

    modified:   simPilotHarvest.R

--- simPilotHarvest.R
+++ simPilotHarvest.R
@@ -36,75 +36,83 @@ studyArea <- raster("../data/studyArea.tif")
 ### harvesting parameters (to be updated)
 harvestedCoverTypes <- c("EN", "PG")
 maturityCoverTypes <- c(EN = 90, PG = 76)
-harvestingRates <- c(EN = 0.015, PG = 0.015)
-#harvestingRates <- 1/maturityCoverTypes
+harvestingRates <- list(EN = c(0.0051, 0.0083, 0.01, 0.011, 0.0125, 0.015),
+                        PG = c(0.0051, 0.0083, 0.01, 0.011, 0.0125, 0.015))
+
+
 
 ID <- coverTypesLevels[match(harvestedCoverTypes, coverTypesLevels$descrip), "ID"]
 
-prescriptions <- data.frame(ID = ID,
-                            coverType = harvestedCoverTypes,
-                            maturity = maturityCoverTypes,
-                            rate =harvestingRates)
 
-coverTypes[coverTypes %in% ID == F] <- NA
-
-
-####################################################################################################
-####################################################################################################
-######
-######      simulations
-source("../scripts/simHarvestFnc.R")
-####################################################################################################
-###################################################################################################
-### fetching fire simulations
-x <- list.files(outputFolder)
-x <- x[grep(".RData", x)]
-simInfo <- gsub(".RData", "", x)
-simInfo <- strsplit(simInfo, "_")
-scenario <- as.character(lapply(simInfo, function(x) x[[2]]))
-replicates <- as.character(lapply(simInfo, function(x) x[3]))
-####################################################################################################
-####################################################################################################
-
-
-
-
-require(doSNOW)
-require(parallel)
-clusterN <-  max(1, floor(0.9*detectCores()))  ### choose number of nodes to add to cluster.
-# #######
-cl = makeCluster(clusterN, outfile = "") ## 
-registerDoSNOW(cl)
-
-
-##
-
-foreach(i = seq_along(x)) %dopar%  {
-
-    require(raster)
-    print(paste("simulating replicate", replicates[i], "; scenario", scenario[i]))
+for (h in seq_along(harvestingRates[[1]])) { ### this loops through harvesting threatments 
     
-    output <- get(load(paste(outputFolder, x[i], sep ="/")))
-    timeSinceFire <- output[["tsf"]]
-    rm(output)
-    
-    timeSinceFire[is.na(coverTypes)] <- NA
-    
-    outputHarvest <- simHarvest(coverTypes,
-                                timeSinceFire,
-                                prescriptions)
-    
-    fName <- paste0(getwd(), "/simHarvest", prescriptions[1,4], "_", scenario[i], "_", replicates[i], ".RData")
-    print("##############################################################")
-    print("##############################################################")
-    print(paste0("Simulation #", i, " completed.")) 
+    hr <- as.numeric(lapply(harvestingRates, function(x) x[[h]]))
+    names(hr) <- names(harvestingRates)
 
-    save(outputHarvest, file = fName)
-    print(paste0("Done! Outputs saved to file '", fName, "'"))
-    print("##############################################################")
+    prescriptions <- data.frame(ID = ID,
+                                coverType = harvestedCoverTypes,
+                                maturity = maturityCoverTypes,
+                                rate = hr)
     
+    coverTypes[coverTypes %in% ID == F] <- NA
+    
+    
+    ####################################################################################################
+    ####################################################################################################
+    ######
+    ######      simulations
+    source("../scripts/simHarvestFnc.R")
+    ####################################################################################################
+    ###################################################################################################
+    ### fetching fire simulations
+    x <- list.files(outputFolder)
+    x <- x[grep(".RData", x)]
+    simInfo <- gsub(".RData", "", x)
+    simInfo <- strsplit(simInfo, "_")
+    scenario <- as.character(lapply(simInfo, function(x) x[[2]]))
+    replicates <- as.character(lapply(simInfo, function(x) x[3]))
+    ####################################################################################################
+    ####################################################################################################
+    
+    
+    
+    
+    require(doSNOW)
+    require(parallel)
+    clusterN <-  max(1, floor(0.9*detectCores()))  ### choose number of nodes to add to cluster.
+    # #######
+    cl = makeCluster(clusterN, outfile = "") ## 
+    registerDoSNOW(cl)
+    
+    
+    ##
+    
+    foreach(i = seq_along(x)) %dopar%  {
+    
+        require(raster)
+        print(paste("simulating replicate", replicates[i], "; scenario", scenario[i]))
+        
+        output <- get(load(paste(outputFolder, x[i], sep ="/")))
+        timeSinceFire <- output[["tsf"]]
+        rm(output)
+        
+        timeSinceFire[is.na(coverTypes)] <- NA
+        
+        outputHarvest <- simHarvest(coverTypes,
+                                    timeSinceFire,
+                                    prescriptions)
+        
+        fName <- paste0(getwd(), "/simHarvest", prescriptions[1,4], "_", scenario[i], "_", replicates[i], ".RData")
+        print("##############################################################")
+        print("##############################################################")
+        print(paste0("Simulation #", i, " completed.")) 
+    
+        save(outputHarvest, file = fName)
+        print(paste0("Done! Outputs saved to file '", fName, "'"))
+        print("##############################################################")
+        
+    }
+    stopCluster(cl)
+
 }
-stopCluster(cl)
-
-
 #######
